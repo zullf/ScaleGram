ba9d0cbe2894c3572d3fde39fb51ca8268df1ddd
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-06-07 20:37:18 +0700

    feat: implement offline-first architecture using expo-sqlite for feed caching

--- src/data/repositories/postRepositoryImpl.js
+++ src/data/repositories/postRepositoryImpl.js
@@ -7,22 +7,30 @@ import {
   getDocs, 
   addDoc 
 } from 'firebase/firestore';
+
 import { 
   ref, 
   uploadBytes, 
   getDownloadURL 
 } from 'firebase/storage';
 import { db, storage } from '../../config/firebase'; 
+import { createSQLiteDataSource } from '../datasources/sqlite/sqliteDataSource';
+
+// 1. Inisialisasi Brankas Lokal SQLite
+const localDb = createSQLiteDataSource();
+localDb.initDB();
 
 export const postRepository = {
   
   /**
-   * Mengambil feed post dengan sistem infinite scroll
+   * Mengambil feed post dengan sistem offline-first & infinite scroll
    * @param {number} pageSize - Jumlah item per load
    * @param {object|null} lastVisible - Dokumen terakhir dari query sebelumnya
    */
   getPosts: async (pageSize = 10, lastVisible = null) => {
     try {
+
+      // Skenario A: Tarik data terbaru dari Firebase (Online)
       let q = query(
         collection(db, 'posts'),
         orderBy('createdAt', 'desc'),
@@ -41,9 +49,23 @@ export const postRepository = {
       
       const lastDoc = snapshot.docs[snapshot.docs.length - 1];
 
+      // MAOFFLINE: Fotokopi data fresh ke Brankas Lokal (Hanya halaman pertama)
+      if (!lastVisible && posts.length > 0) {
+        localDb.cachePosts(posts);
+      }
+
       return { posts, lastDoc };
     } catch (error) {
-      console.error("Error fetching posts:", error);
+      console.log("Gagal konek ke Firebase (Mungkin Offline):", error.message);
+      
+      // OFFLINE: Kalau Offline/Error, selamatkan dengan data di Brankas!
+      if (!lastVisible) {
+        console.log("Beralih ke Mode Offline...");
+        const cachedPosts = localDb.getCachedPosts();
+        return { posts: cachedPosts, lastDoc: null };
+      }
+
+      // Kalau lagi infinite scroll (halaman 2 dst) dan tiba-tiba offline, lemparkan errornya
       throw error;
     }
   },
--- src/presentation/screens/feed/FeedScreen.js
+++ src/presentation/screens/feed/FeedScreen.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { postRepository } from '../../../data/repositories/postRepositoryImpl';
 
 export default function FeedScreen() {
-
+  
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Sedang Mengetes Feed & Scroll. Cek Terminal!</Text>
